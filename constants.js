c21dddbc9dba79d81f617c0d02af9cc0002fdfb4
Austin <austin.peters@kineticdata.com>
2018-06-05 06:30:35 -0500

    Initial commit.

---
+++ constants.js
@@ -0,0 +1,44 @@
+import { List, Map, Record } from 'immutable';
+
+export const PLACED_MULTIPLIER_PAYOUTS = {
+  "2": 1,
+  "4": 1,
+  "5": 1,
+  "6": 1,
+  "8": 1,
+  "9": 1,
+  "10": 1
+}
+
+export const COME_MULTIPLIER_PAYOUTS = {
+  "2": 1,
+  "4": 1,
+  "5": 1,
+  "6": 1,
+  "8": 1,
+  "9": 1,
+  "10": 1 
+}
+
+export const DONTCOME_MULTIPLIER_PAYOUTS = {
+  "2": 1,
+  "4": 1,
+  "5": 1,
+  "6": 1,
+  "8": 1,
+  "9": 1,
+  "10": 1 
+}
+
+export const DONTPASS_MULTIPLIER_PAYOUTS = {
+  "2": 1,
+  "4": 1,
+  "5": 1,
+  "6": 1,
+  "8": 1,
+  "9": 1,
+  "10": 1 
+}
+
+export const PASS_MULTIPLIER_PAYOUTS = COME_MULTIPLIER_PAYOUTS;
+export const DONTPASS_MULTIPLIER_PAYOUTS = DONTCOME_MULTIPLIER_PAYOUTS;
